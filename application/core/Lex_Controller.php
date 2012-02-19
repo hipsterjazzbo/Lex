d0a9994f005d48c2f5ab97502140268abc1ca356
Caleb Fidecaro <calebfidecaro@gmail.com>
2012-02-19 19:33:09 +1300

    Fix order error

--- application/core/Lex_Controller.php
+++ application/core/Lex_Controller.php
@@ -14,14 +14,14 @@ class Lex_Controller extends CI_Controller {
 		$current_language = $this->session->userdata('current_language');
 		$languages        = $this->session->userdata('languages');
 
-		if ( ! $current_language)
-		{
-			$this->session->set_userdata('current_language', array_shift($languages));
-		}
-
 		if ( ! $languages)
 		{
 			$this->session->set_userdata('languages', $this->language_model->get_languages());
+		}
+
+		if ( ! $current_language)
+		{
+			$this->session->set_userdata('current_language', array_shift($languages));
 		}
 	}
 
